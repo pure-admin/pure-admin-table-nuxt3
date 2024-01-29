a46f69798abc42d0a9f036789e470426d63e8b29
xiaoxian521 <1923740402@qq.com>
2024-01-29 13:13:54 +0800

    chore: update `@pureadmin/table^3.0.2`

--- plugins/config.ts
+++ plugins/config.ts
@@ -3,5 +3,5 @@ import PureTable from "@pureadmin/table";
 export default defineNuxtPlugin(() => {
   const { use, $nuxt } = useNuxtApp().vueApp;
   // 全局注册@pureadmin/table
-  use(PureTable, { i18n: $nuxt.$i18n });
+  use(PureTable, { i18n: $nuxt.$i18n, ssr: true });
 });
